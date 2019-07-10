1056485ef0f49bacb33b133818323ac41c874f7a
maitreyee290897 <maitreyee290897@gmail.com>
2019-07-10 11:23:54 +0530

    10july2019_getpost

---
+++ demoWeb/src/com/controller/AbcServlet.java
@@ -0,0 +1,33 @@
+package com.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class AbcServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private int i=0;
+    public AbcServlet() {
+        super();
+        
+    }
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("Servlet initiated");
+	}
+
+	
+	public void destroy() {
+		System.out.println("servlet destroyed");
+	}
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("servlet request came : " + ++i);
+	}
+
+}
